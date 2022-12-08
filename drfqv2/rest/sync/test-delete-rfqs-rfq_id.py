43113d83f7d40c497c76a245df45d97c56d66376
Elliot <elliot-parker@hotmail.com>
2022-12-08 12:39:05 +1000

    add drfqv2 rest sync examples

---
+++ drfqv2/rest/sync/test-delete-rfqs-rfq_id.py
@@ -0,0 +1,50 @@
+# built ins
+import base64
+import hmac
+import time
+
+# installed
+import requests
+
+access_key = '<access-key>'
+secret_key = b'<secret-key>'
+
+
+def sign_request(secret_key, method, path, body):
+    signing_key = base64.b64decode(secret_key)
+    timestamp = str(int(time.time() * 1000)).encode('utf-8')
+    message = b'\n'.join([timestamp, method.upper(), path, body])
+    digest = hmac.digest(signing_key, message, 'sha256')
+    signature = base64.b64encode(digest)
+    return timestamp, signature
+
+
+# Request Host
+host = 'https://api.testnet.paradigm.trade'
+
+# DELETE /v2/drfq/rfqs/{rfq_id}
+method = 'DELETE'
+path = '/v2/drfq/rfqs/123'
+
+payload = ''
+
+timestamp, signature = sign_request(secret_key=secret_key,
+                                    method=method.encode('utf-8'),
+                                    path=path.encode('utf-8'),
+                                    body=payload.encode('utf-8'),
+                                    )
+
+headers = {
+          'Paradigm-API-Timestamp': timestamp,
+          'Paradigm-API-Signature': signature,
+          'Authorization': f'Bearer {access_key}'
+           }
+
+# Send request
+response = requests.delete(
+    host+path,
+    headers=headers
+    )
+
+print(response.status_code)
+print(response.text)
